5b09a5522dbc9a8802e5218aefe09ba9d9c5803e
Edmund Higham <edhigham@gmail.com>
2021-12-02 23:00:58 -0500

    add test + follow hpack suggestions for default-extensions

--- 2021/test/DiveSpec.hs
+++ 2021/test/DiveSpec.hs
@@ -6,10 +6,16 @@ import Test.Tasty.HUnit
 import Day2.Dive ( Command ( .. ) )
 
 tests :: TestTree
-tests = testGroup "Dive unit tests" [ testRead ]
+tests = testGroup "Dive unit tests" [ testRead, testReadId ]
 
 testRead = testGroup "Testing instance Read Command"
     [   testCase "forward" $ read "forward 5" @?= Forward 5
     ,   testCase "down"    $ read "down 8"    @?= Down 8
     ,   testCase "up"      $ read "up 3"      @?= Up 3
+    ]
+
+testReadId = testGroup "Testing show . read = id"
+    [ testCase s $ let str = s ++ " 5" in
+        (show . (read :: String -> Command)) str @?= str
+    | s <- ["forward", "down", "up"]
     ]
