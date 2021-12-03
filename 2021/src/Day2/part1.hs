3072b66b620b60ad66dd68dcd230671521641f49
Edmund Higham <edhigham@gmail.com>
2021-12-02 20:20:41 -0500

    more renaming

--- 2021/src/Day2/part1.hs
+++ 2021/src/Day2/part1.hs
@@ -1,11 +1,15 @@
 import qualified System.Environment as Env ( getArgs )
 import Text.Printf ( printf )
+
 import Day2.Dive ( Command, part1 )
 
 main :: IO ()
 main = do
     [input] <- Env.getArgs
     contents <- readFile input
-    let commands = map read (lines contents) :: [Command]
-        (x, y)   = foldl (flip part1) (0, 0) commands
-    printf "Position:\n\thorizontal = %d\n\tdepth = %d\n\tproduct = %d\n" x y (x * y)
+    let commands            = map read (lines contents) :: [Command]
+        (horizontal, depth) = foldl (flip part1) (0, 0) commands
+    printf "Position:\n\thorizontal = %d\n\tdepth = %d\n\tproduct = %d\n"
+        horizontal
+        depth
+        (horizontal * depth)
--- 2021/src/Day2/part2.hs
+++ 2021/src/Day2/part2.hs
@@ -1,11 +1,16 @@
 import qualified System.Environment as Env ( getArgs )
 import Text.Printf ( printf )
+
 import Day2.Dive ( Command, part2 )
 
 main :: IO ()
 main = do
     [input] <- Env.getArgs
     contents <- readFile input
-    let commands  = map read (lines contents) :: [Command]
-        (x, y, a) = foldl (flip part2) (0, 0, 0) commands
-    printf "Position:\n\thorizontal = %d\n\tdepth = %d\n\taim = %d\n\tproduct = %d\n" x y a (x * y)
+    let commands                 = map read (lines contents) :: [Command]
+        (horizontal, depth, aim) = foldl (flip part2) (0, 0, 0) commands
+    printf "Position:\n\thorizontal = %d\n\tdepth = %d\n\taim = %d\n\tproduct = %d\n"
+        horizontal
+        depth
+        aim
+        (horizontal * depth)
