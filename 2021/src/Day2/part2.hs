47ac8c84d934e065daa5fc8f6ea8d7a28938d5be
Edmund Higham <edhigham@gmail.com>
2021-12-02 20:14:29 -0500

    fix whitespace

--- 2021/src/Day2/part2.hs
+++ 2021/src/Day2/part2.hs
@@ -6,6 +6,6 @@ main :: IO ()
 main = do
     [input] <- Env.getArgs
     contents <- readFile input
-    let commands = map read (lines contents) :: [Command]
-        (x, y, a)   = foldl (flip part2) (0, 0, 0) commands
+    let commands  = map read (lines contents) :: [Command]
+        (x, y, a) = foldl (flip part2) (0, 0, 0) commands
     printf "Position:\n\thorizontal = %d\n\tdepth = %d\n\taim = %d\n\tproduct = %d\n" x y a (x * y)
