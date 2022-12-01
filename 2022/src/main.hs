7ac0fbd1f350d0b9d129fa52b5f295574a57b75a
Edmund Higham <edhigham@gmail.com>
2022-12-01 15:23:32 -0500

    use website link

--- 2022/src/main.hs
+++ 2022/src/main.hs
@@ -2,13 +2,14 @@ import Options.Applicative
 
 import Control.Monad (join)
 
+
 import Advent.Day1 qualified as Day1
 
 main :: IO ()
 main = join $ execParser (info (dayN <**> helper) description)
   where
     description = fullDesc
-      <> progDesc "Solutions to Advent of Code 2022"
+      <> progDesc "Solutions to https://adventofcode.com/2022"
       <> footer "Copyright (C) 2022 Edmund Higham <edhigham@gmail.com>"
 
     dayN = subparser $ mkCommand "day1" Day1.main
