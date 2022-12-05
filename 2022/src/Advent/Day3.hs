4c26721162e45a98e432e3cfc70178c137d8ae2a
Edmund Higham <edhigham@gmail.com>
2022-12-05 14:31:34 -0500

    Day3 part 2

--- 2022/src/Advent/Day3.hs
+++ 2022/src/Advent/Day3.hs
@@ -2,10 +2,13 @@
 module Advent.Day3 (main) where
 
 import Control.Applicative       ((<|>))
+import Control.Arrow             ((***))
 import Control.Exception         (throw)
+import Control.Monad             (join)
 import Data.Char                 (isLower, ord)
 import Data.Functor              (void)
-import Data.List                 (intersect, nub)
+import Data.List.Split           (chunksOf)
+import Data.Set                  qualified as S
 import Data.Text.IO              qualified as T
 import Text.Parsec               (parse)
 import Text.Parsec.Text          (Parser)
@@ -77,16 +80,73 @@ import Advent.Share.ParsecUtils  (ParseException(..))
 part1 :: [[Char]] -> IO ()
 part1 = printf "Sum of priorities of common item types = %d\n"
       . sum
-      . map (sum . map priority . nub . uncurry intersect . compartments)
+      . map ( sum
+            . map priority
+            . S.toList
+            . uncurry S.intersection
+            . join (***) S.fromList
+            . compartments
+            )
   where
-    priority c = ord c - if isLower c then 96 else 38
-
     compartments = splitAt =<< (`quot` 2) . length
 
 
--- |
+-- | Part Two
+-- As you finish identifying the misplaced items, the Elves come to you with
+-- another issue.
+--
+-- For safety, the Elves are divided into groups of three. Every Elf carries a
+-- badge that identifies their group. For efficiency, within each group of three
+-- Elves, the badge is the only item type carried by all three Elves. That is,
+-- if a group's badge is item type B, then all three Elves will have item type B
+-- somewhere in their rucksack, and at most two of the Elves will be carrying
+-- any other item type.
+--
+-- The problem is that someone forgot to put this year's updated authenticity
+-- sticker on the badges. All of the badges need to be pulled out of the
+-- rucksacks so the new authenticity stickers can be attached.
+--
+-- Additionally, nobody wrote down which item type corresponds to each group's
+-- badges. The only way to tell which item type is the right one is by finding
+-- the one item type that is common between all three Elves in each group.
+--
+-- Every set of three lines in your list corresponds to a single group, but each
+-- group can have a different badge item type. So, in the above example, the
+-- first group's rucksacks are the first three lines:
+--
+-- @
+-- vJrwpWtwJgWrhcsFMMfFFhFp
+-- jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+-- PmmdzqPrVvPwwTWBwg
+-- @
+--
+-- And the second group's rucksacks are the next three lines:
+--
+-- @
+-- wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+-- ttgJtRGJQctTZtZT
+-- CrZsJsPPZsGzwwsLwLmpwMDw
+-- @
+--
+-- In the first group, the only item type that appears in all three rucksacks is
+-- lowercase r; this must be their badges. In the second group, their badge item
+-- type must be Z.
+--
+-- Priorities for these items must still be found to organize the sticker
+-- attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for
+-- the second group. The sum of these is 70.
+--
+-- Find the item type that corresponds to the badges of each three-Elf group.
+-- What is the sum of the priorities of those item types?
 part2 :: [[Char]] -> IO ()
-part2 _ = printf "not implemented\n"
+part2 = printf "Sum of priorities of item badges of each 3-elf group = %d\n"
+      . sum
+      . concatMap (map priority . S.toList . foldl1 S.intersection . map S.fromList)
+      . chunksOf 3
+
+
+priority ::  Char -> Int
+priority c = ord c - if isLower c then 96 else 38
 
 
 inputParser :: Parser [[Char]]
