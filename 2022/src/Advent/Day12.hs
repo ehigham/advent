7a213a35903ef6f77712dddd65ec221a1d96a27b
Edmund Higham <edhigham@gmail.com>
2022-12-13 15:21:26 -0500

    day 12

--- 2022/src/Advent/Day12.hs
+++ 2022/src/Advent/Day12.hs
@@ -7,6 +7,7 @@ import Control.Monad.Extra       (unlessM)
 import Control.Monad.Loops       (whileJust', whileM_)
 import Control.Monad.ST          (runST)
 import Data.Function             (on)
+import Data.List                 (sort)
 import Data.PQueue.Min           qualified as MinQ
 import Data.Map.Strict           qualified as Map
 import Data.STRef                (newSTRef, readSTRef, modifySTRef', writeSTRef)
@@ -87,13 +88,65 @@ part1 = printf "Fewest steps required to move from S to E = %d\n"
     start  = unsafeIndexOf $ Elevation 'S'
     finish = unsafeIndexOf $ Elevation 'E'
 
+-- | Part 2
+--
+-- As you walk up the hill, you suspect that the Elves will want to turn this
+-- into a hiking trail. The beginning isn't very scenic, though; perhaps you can
+-- find a better starting point.
+--
+-- To maximize exercise while hiking, the trail should start as low as possible:
+-- elevation a. The goal is still the square marked E. However, the trail should
+-- still be direct, taking the fewest steps to reach its goal. So, you'll need
+-- to find the shortest path from any square at elevation a to the square marked
+-- E.
+--
+-- Again consider the example from above:
+-- @
+-- Sabqponm
+-- abcryxxl
+-- accszExk
+-- acctuvwj
+-- abdefghi
+-- @
+--
+-- Now, there are six choices for starting position (five marked a, plus the
+-- square marked S that counts as being at elevation a). If you start at the
+-- bottom-left square, you can reach the goal most quickly:
+--
+-- @
+-- ...v<<<<
+-- ...vv<<^
+-- ...v>E^^
+-- .>v>>>^^
+-- >^>>>>>^
+-- @
+--
+-- This path reaches the goal in only 29 steps, the fewest possible.
+--
+-- What is the fewest steps required to move starting from any square
+-- with elevation a to the location that should get the best signal?
+part2 :: Matrix Elevation -> IO ()
+part2 = printf "Fewest steps required to move from any a to E = %d\n"
+      . liftA3 minShortestPath finish id starts
+  where
+    starts = indexOf (Elevation 'S') <> indexOf (Elevation 'a')
+    finish = unsafeIndexOf $ Elevation 'E'
+
+    minShortestPath finish' mat =
+        head
+        . dropWhile (== 0)
+        . sort
+        . map (\start' -> length $ shortestPath start' finish' mat)
+
+
 
 unsafeIndexOf :: Eq a => a -> Matrix a -> (Int, Int)
-unsafeIndexOf a m =
-    head $ filter
-        ((== a) . (m Mat.!))
-        [(i, j) | j <- [1..(Mat.ncols m)], i <- [1..(Mat.nrows m)]]
+unsafeIndexOf = (head .) . indexOf
 
+indexOf :: Eq a => a -> Matrix a -> [(Int, Int)]
+indexOf a m = filter
+                ((== a) . (m Mat.!))
+                [(i, j) | j <- [1..(Mat.ncols m)], i <- [1..(Mat.nrows m)]]
 
 shortestPath :: Enum a
              => (Int, Int)
@@ -157,11 +210,6 @@ dijkstra adj start finish = runST $ do
         return min'
 
     push q = modifySTRef' q . MinQ.insert
-
-
--- | Part 2
-part2 :: a -> IO ()
-part2 _ = printf "not implemented\n"
 
 
 inputParser :: Parser (Matrix Elevation)
