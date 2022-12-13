c6cdfd49b3e3303a5efc4719d112e61249ce9ce7
Edmund Higham <edhigham@gmail.com>
2022-12-13 17:57:18 -0500

    day 13

--- 2022/src/Advent/Day13.hs
+++ 2022/src/Advent/Day13.hs
@@ -2,6 +2,10 @@ module Advent.Day13 (main) where
 
 import Control.Applicative       ((<|>))
 import Control.Exception         (throw)
+import Data.Either.Extra         (fromRight')
+import Data.Function             (on)
+import Data.List                 (elemIndex, sort)
+import Data.Maybe                (fromJust)
 import Data.Text.IO              qualified as T
 import Text.Parsec               (parse)
 import Text.Parsec.Text          (Parser)
@@ -165,8 +169,67 @@ part1 = printf "Sum of indicies in right order = %d\n"
 
 
 -- | Part 2
-part2 :: a -> IO ()
-part2 _ = printf "not implemented\n"
+--
+-- Now, you just need to put all of the packets in the right order. Disregard
+-- the blank lines in your list of received packets.
+--
+-- The distress signal protocol also requires that you include two additional
+-- divider packets:
+--
+-- @
+-- [[2]]
+-- [[6]]
+-- @
+--
+-- Using the same rules as before, organize all packets - the ones in your list
+-- of received packets as well as the two divider packets - into the correct
+-- order.
+--
+-- For the example above, the result of putting the packets in the correct order
+-- is:
+--
+-- @
+-- []
+-- [[]]
+-- [[[]]]
+-- [1,1,3,1,1]
+-- [1,1,5,1,1]
+-- [[1],[2,3,4]]
+-- [1,[2,[3,[4,[5,6,0]]]],8,9]
+-- [1,[2,[3,[4,[5,6,7]]]],8,9]
+-- [[1],4]
+-- [[2]]
+-- [3]
+-- [[4,4],4,4]
+-- [[4,4],4,4,4]
+-- [[6]]
+-- [7,7,7]
+-- [7,7,7,7]
+-- [[8,7,6]]
+-- [9]
+-- @
+--
+-- Afterward, locate the divider packets. To find the decoder key for this
+-- distress signal, you need to determine the indices of the two divider packets
+-- and multiply them together. (The first packet is at index 1, the second
+-- packet is at index 2, and so on.) In this example, the divider packets are
+-- 10th and 14th, and so the decoder key is 140.
+--
+-- Organize all of the packets into the correct order. What is the decoder key
+-- for the distress signal?
+part2 :: [(Packet, Packet)] -> IO ()
+part2 packets = printf "Product of indicies of the dividers = %d\n" $
+    let sorted = sort (joinPairs $ dividers:packets) in
+        fromJust $ ((*) `on` (+1))
+                <$> elemIndex (fst dividers) sorted
+                <*> elemIndex (snd dividers) sorted
+
+  where
+    dividers :: (Packet, Packet)
+    dividers = head . fromRight' $ parse inputParser "dividers" "[[2]]\n[[6]]\n"
+
+    joinPairs :: [(Packet, Packet)] -> [Packet]
+    joinPairs = concatMap (\(a,b) -> [a, b])
 
 
 data Packet = I Int | L [Packet]
